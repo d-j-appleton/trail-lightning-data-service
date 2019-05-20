b4fbdecbba419808eb5688194aef64b9024327b5
Dave Appleton <davidappleton@brightgen.com>
2019-05-20 14:08:43 +0100

    Commit for the update implementing Stage 2

--- trail-lightning-data-service/force-app/main/default/aura/accEdit/accEditController.js
+++ trail-lightning-data-service/force-app/main/default/aura/accEdit/accEditController.js
@@ -1,5 +1,18 @@
 ({
-    myAction : function(component, event, helper) {
-
+    handleSaveContact : function(cmp, event, helper) {
+        var recordLoader = cmp.find("recordLoader");
+        recordLoader.saveRecord($A.getCallback(function(saveResult) {
+            if (saveResult.state === "ERROR") {
+                var errMsg = "";
+                // saveResult.error is an array of errors, 
+                // so collect all errors into one message
+                for (var i = 0; i < saveResult.error.length; i++) {
+                    errMsg += saveResult.error[i].message + "\n";
+                }
+                cmp.set("v.recordSaveError", errMsg);
+            } else {
+                cmp.set("v.recordSaveError", "");
+            }
+        }));
     }
 })
